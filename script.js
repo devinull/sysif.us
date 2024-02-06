20ae7dccbe011951c63cbc434b47942761c2295a
DevNull <devin.ronneberg@gmail.com>
2024-02-05 16:49:23 -0800

    add site files

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Fetch the text file
+    fetch('text.txt')
+    .then(response => response.text())
+    .then(text => {
+        typeText(text);
+    })
+    .catch(error => console.error('Error fetching text:', error));
+
+    const textElement = document.getElementById('text');
+    let textIndex = 0;
+    const typingSpeed = 50; // in milliseconds
+
+    function typeText(text) {
+        const typingInterval = setInterval(function() {
+            if (textIndex < text.length) {
+                // Remove the previous cursor
+                const previousCursor = textElement.querySelector('.cursor');
+                if (previousCursor) {
+                    textElement.removeChild(previousCursor);
+                }
+                
+                textElement.textContent += text[textIndex];
+                
+                // Add the new cursor
+                const cursorSpan = document.createElement('span');
+                cursorSpan.textContent = '_';
+                cursorSpan.classList.add('cursor');
+                textElement.appendChild(cursorSpan);
+
+                textIndex++;
+            } else {
+                clearInterval(typingInterval);
+            }
+        }, typingSpeed);
+    }
+});
