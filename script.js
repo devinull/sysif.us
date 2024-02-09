127e6fa6fe71acf6761dbba4c2094206e27a84d6
DevNull <devin.ronneberg@gmail.com>
2024-02-09 15:05:06 -0800

    rewrite js + update filenames
    refactor js to load text files properly

--- script.js
+++ script.js
@@ -1,57 +1,65 @@
-function reloadPage() {
-    window.location.reload();
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-    const baseUrl = 'https://raw.githubusercontent.com/devinull/psycho.site/main/text/onImprisonment/trapped.';
+    const baseUrl = 'https://raw.githubusercontent.com/devinull/psycho.site/main/text/';
+    let folderUrl = 'onFreedom/freedom.';
+    const numText = 92;
+    let urlNumber = getRandomNumber(1, numText);
+    let isFetching = false; // Flag to track if text is currently being fetched
+    let typingTimeout; // Timeout for typing animation
+
     const metadataElement = document.getElementById('metadata');
     const textElement = document.getElementById('text');
-    const numText = 92;
     let animationComplete = false;
-    let urlNumber = getRandomNumber(1, numText); // Get a random number between 1 and 255
-    let audioLoaded = false; // Flag to track audio loading state
 
     // Audio element for typing sound effect
-    const typingSound = new Audio('https://raw.githubusercontent.com/devinull/psycho.site/main/audio/thock.mp3'); // Adjust the file name and path or url as needed
+    const typingSound = new Audio('https://raw.githubusercontent.com/devinull/psycho.site/main/audio/thock.mp3');
 
-    // Function to fetch text from the URL
+    // Fetch text from the URL
     function fetchTextFromUrl(url) {
+        // Clear the text content before fetching new text
+        textElement.textContent = '';
+        // Clear typing timeout if it exists
+        clearTimeout(typingTimeout);
+        // If text is already being fetched, return
+        if (isFetching) return;
+        isFetching = true;
+
         fetch(url)
         .then(response => response.text())
         .then(text => {
             typeText(text);
         })
-        .catch(error => console.error('Error fetching text:', error));
+        .catch(error => console.error('Error fetching text:', error))
+        .finally(() => {
+            isFetching = false; // Reset the flag after fetching is complete
+        });
     }
 
-    // Function to generate a random number between min and max (inclusive)
+    // Generate a random number between min and max (inclusive)
     function getRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    // Function to generate a random typing speed between min and max (inclusive)
+    // Generate a random typing speed between min and max (inclusive)
     function getRandomTypingSpeed(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    // Function to type the text
+    // Type the text
     function typeText(text) {
         let textIndex = 0;
         textElement.textContent = ''; // Clear previous text
-        typeNextLetter();
 
         // Set the metadata text
         metadataElement.textContent = 'thought(' + urlNumber.toString(2) + ') >';
-        
+
         function typeNextLetter() {
             if (textIndex < text.length) {
-                // Check if the audio is playing
-                if (!typingSound.paused) {
-                    // If the audio is playing, rewind it to the beginning
-                    typingSound.currentTime = 0;
-                } else {
-                    // If the audio is not playing, start playing it
-                    typingSound.play();
+                typingSound.play();
+                // Check for line breaks and new lines
+                if (text[textIndex] === '\n') {
+                    textElement.innerHTML += '<br>';
+                } else if (text[textIndex] !== '\r') {
+                    textElement.innerHTML += text[textIndex];
                 }
 
                 // Remove the previous cursor
@@ -59,17 +67,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 if (previousCursor) {
                     textElement.removeChild(previousCursor);
                 }
-
-                // Check for line breaks and new lines
-            if (text[textIndex] === '\n') {
-                // If it's a new line, append a <br> element
-                textElement.innerHTML += '<br>';
-            } else if (text[textIndex] === '\r') {
-                // Ignore carriage return characters
-            } else {
-                // Add the new character
-                textElement.innerHTML += text[textIndex];
-            }
 
                 // Add the new cursor
                 const cursorSpan = document.createElement('span');
@@ -81,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 const typingSpeed = getRandomTypingSpeed(35, 100);
 
                 // Wait for the next letter with the calculated typing speed
-                setTimeout(() => {
+                typingTimeout = setTimeout(() => {
                     textIndex++;
                     typeNextLetter();
                 }, typingSpeed);
@@ -91,18 +88,39 @@ document.addEventListener("DOMContentLoaded", function() {
                 setTimeout(function() {
                     if (animationComplete) {
                         // Increment URL number and fetch next text
-                        urlNumber++;
-                        if (urlNumber > numText) {
-                            urlNumber = getRandomNumber(0, numText); // Set to a random value between 0 and 255
-                        }
-                        const nextUrl = baseUrl + urlNumber + '.txt';
+                        urlNumber = (urlNumber % numText) + 1; // Ensure it cycles through numText
+                        const nextUrl = baseUrl + folderUrl + urlNumber + '.txt';
                         fetchTextFromUrl(nextUrl);
                     }
                 }, 5000); // 5000 milliseconds = 5 seconds
             }
         }
+
+        typeNextLetter(); // Start typing
     }
 
-    // Fetch text from the initial URL
-    fetchTextFromUrl(baseUrl + urlNumber + '.txt');
+    // Navigation functions
+    function changeTopic(newFolderUrl) {
+        if (isFetching) return; // Prevent changing topic while text is being fetched
+        folderUrl = newFolderUrl;
+        urlNumber = getRandomNumber(1,numText);
+        const nextUrl = baseUrl + folderUrl + urlNumber + '.txt';
+        fetchTextFromUrl(nextUrl);
+    }
+
+    // Attach event listeners to navigation links
+    document.getElementById('onSelf').addEventListener('click', function() {
+        changeTopic('onSelf/self.');
+    });
+
+    document.getElementById('onSpirit').addEventListener('click', function() {
+        changeTopic('onSpirit/spirit.');
+    });
+
+    document.getElementById('onFreedom').addEventListener('click', function() {
+        changeTopic('onFreedom/freedom.');
+    });
+
+    // Initial fetch
+    fetchTextFromUrl(baseUrl + folderUrl + urlNumber + '.txt');
 });
